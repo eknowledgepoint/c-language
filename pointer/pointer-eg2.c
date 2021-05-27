976d0e62a09e6c250061e727eecc2e958ac4d08a
Vishnu Kant Maurya <vishnukant0077@gmail.com>
2021-05-27 11:41:27 +0530

    update

---
+++ pointer/pointer-eg2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int* pc, c;
+   // explanation of address of a variable is not changed if the value changed.
+   c = 22;
+   printf("Address of c: %p\n", &c);
+   printf("Value of c: %d\n\n", c);  // 22
+   
+   pc = &c;
+   printf("Address of pointer pc: %p\n", pc);
+   printf("Content of pointer pc: %d\n\n", *pc); // 22
+   
+   c = 11;
+   printf("Address of pointer pc: %p\n", pc);
+   printf("Content of pointer pc: %d\n\n", *pc); // 11
+
+   return 0;
+}
